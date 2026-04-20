0b98c75ee66466088847962db7be3bf7cbaeae4c
Arthurluis03 <arthurluis.alml@gmail.com>
2026-04-20 17:03:26 -0300

    fiz de duas formas, for e do while, mais apropriado é while/do while
    Refactor password prompt logic to use a do-while loop for attempts.

--- Ex/ex5/script.js
+++ Ex/ex5/script.js
@@ -1,1 +1,32 @@
+// let senha = prompt("Defina sua senha:")
+// let tentativa
 
+// for (tentativa = 3; tentativa >= 1; tentativa--) {
+//     alert("Faça o login")
+//     let entrada = prompt("Qual a senha?")
+
+//     if (entrada == senha) {
+//         alert("Senha correta, seja bem-vindo!")
+//         break
+//     } else {
+//         alert("Senha incorreta")
+//     }
+
+//     alert(`Você tem ${tentativa - 1} tentativa(s) restante(s)`)
+// }
+
+
+let senha = prompt("Defina sua senha: ")
+let tentativas=3
+ do {  
+       login = prompt("Digite a senha: ")
+       if (senha == login ) {
+        alert("Login bem-sucedido")
+        break
+       } else {
+        alert("Ops, senha errada, tente novamente")
+        tentativas--
+       }
+       alert(`Você tem ${tentativas}, restante(s) `)
+       
+ } while(tentativas>0)
