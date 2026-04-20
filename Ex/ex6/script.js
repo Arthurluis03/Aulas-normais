fc58e5207fbf8d7fb4bb9ae7457c9c497189271e
Arthurluis03 <arthurluis.alml@gmail.com>
2026-04-20 17:04:05 -0300

    timer, usei uma atividade anterior para se basear
    Implement timer simulation for heating process.

--- Ex/ex6/script.js
+++ Ex/ex6/script.js
@@ -1,1 +1,23 @@
+// do {
+//   if (time === bomb) {
+//     alert("Alerta numero invalido, interrompendo BOOOM");
+//     break;
+//   }
+//Crie um programa que simule esse funcionamento do temporizador de
+// aquecimento, garantindo que ele continue funcionando até atingir pelo
+// menos 5 segundos.
+//   alert(time);
+//   time++;
+// } while (time <= limite);
 
+
+
+let time = 0
+let aqc = 5
+   
+for(time = 0; time <=5; time++ ){
+     alert(time)
+     if(time == aqc) {
+        alert("Aquecimento feito com sucesso")
+     }
+}
