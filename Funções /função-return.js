cc6eaea56dad3b05bef8fb03aed57c5fcb070dd4
Arthurluis03 <arthurluis.alml@gmail.com>
2026-04-24 15:52:47 -0300

    Return, aprendendo a utilizar ele na função

---
+++ Funções /função-return.js
@@ -0,0 +1,9 @@
+// deve-se sar uma função com retorno sempre que precisar que ela devolva um valor
+//
+//ao coloca o return na função qer dizer q acaba ali, pos isso nada roda
+//diferents ramificações dentro da função podem ter diferentes retornos.
+// 
+//
+// Resto fica para segunda feira! 
+//
+//
