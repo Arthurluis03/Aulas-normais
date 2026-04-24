85f97b96f664ea5711574ff3b2e0c549c8cce0f5
Arthurluis03 <arthurluis.alml@gmail.com>
2026-04-24 16:10:39 -0300

    Função usando, parametro, if e tudo mais, leia tudo se necessario!

--- Funções /função-return.js
+++ Funções /função-return.js
@@ -7,3 +7,49 @@
 // Resto fica para segunda feira! 
 //
 //
+///// exemplos do return
+
+
+
+
+// aqui podemos colocar os valores, que nem na segunda de x e y, mas escolhi deixar os valores estaticos, como 50 como valor, e 20% 
+function porcentagem(numero, porcentagem) {
+   return numero * (porcentagem/100)
+}
+let resultado = porcentagem(50, 20)
+console.log("Resultado = "+resultado)
+
+
+
+
+
+// nesse casoq que eu fiz, eu perguntei os valores de x e y 
+function calculo(x, y) {
+   return x * y
+}
+let resultado = calculo(+prompt("Digite o valor de x"), +prompt("Digite o valor de y"))
+alert("Resultado = "+resultado)
+
+
+
+//
+
+
+
+// Dessa forma aqui, usamos um sistema bem mais avançado que os outros, usando if, false e true
+
+let numeroidade= +prompt("Qual sua idade")
+let verifica = maiorIdade(numeroidade)
+function maiorIdade(idade){
+    if(idade >=18){
+        return true
+        
+    } else {
+        return false
+    }
+} if(verifica){
+    alert(`Você é maior de idade: sua idade é ${numeroidade}`)
+} else{
+    alert(`Você é menor de idade: sua idade é ${numeroidade}`)
+}
+
